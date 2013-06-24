20e5d5fe9de029c64f97fc0879a5846fd672ca41
jy910.yun <jy910.yun@samsung.com>
2013-06-24 14:28:27 +0900

    embody 'pm' in libdeviced.
    Change-Id: I6c27fc93a205084053814cfc3a59d481e5c3d6c5
    Signed-off-by: Krzysztof Sasiak <k.sasiak@samsung.com>

--- libslp-pm/pm.c
+++ libslp-pm/pm.c
@@ -19,120 +19,23 @@
  *
 */ 
 
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <errno.h>
-#include <linux/limits.h>
-
+#include <dd-display.h>
 #include "pmapi.h"
 #include "pm.h"
 
-#define SOCK_PATH			"/tmp/pm_sock"
-#define SHIFT_UNLOCK			4
-#define SHIFT_UNLOCK_PARAMETER		12
-#define SHIFT_CHANGE_STATE		8
-#define SHIFT_HOLD_KEY_BLOCK		16
-#define TIMEOUT_RESET_BIT		0x80
-
-struct pwr_msg {
-	pid_t pid;
-	unsigned int cond;
-	unsigned int timeout;
-};
-
-static int send_msg(unsigned int s_bits, unsigned int timeout)
-{
-	int rc = 0;
-	int sock;
-	struct pwr_msg p;
-	struct sockaddr_un remote;
-
-	p.pid = getpid();
-	p.cond = s_bits;
-	p.timeout = timeout;
-
-	sock = socket(AF_UNIX, SOCK_DGRAM, 0);
-	if (sock == -1) {
-		ERR("pm socket() failed");
-		return -1;
-	}
-
-	remote.sun_family = AF_UNIX;
-	if(strlen(SOCK_PATH) >= sizeof(remote.sun_path)) {
-		ERR("socket path is vey long");
-		return -1;
-	}
-	strncpy(remote.sun_path, SOCK_PATH, sizeof(remote.sun_path));
-
-	rc = sendto(sock, (void *)&p, sizeof(p), 0, (struct sockaddr *)&remote,
-		    sizeof(struct sockaddr_un));
-	if (rc == -1) {
-		ERR("pm socket sendto() failed");
-	} else
-		rc = 0;
-
-	close(sock);
-	return rc;
-}
-
 API int pm_change_state(unsigned int s_bits)
 {
-	/* s_bits is LCD_NORMAL 0x1, LCD_DIM 0x2, LCD_OFF 0x4, SUSPEND 0x8
-	 * Stage change to NORMAL       0x100
-	 * Stage change to LCDDIM       0x200
-	 * Stage change to LCDOFF       0x400
-	 * Stage change to SLEEP        0x800
-	 * */
-	switch (s_bits) {
-	case LCD_NORMAL:
-	case LCD_DIM:
-	case LCD_OFF:
-	case SUSPEND:
-	case POWER_OFF:
-		break;
-	default:
-		return -1;
-	}
-	return send_msg(s_bits << SHIFT_CHANGE_STATE, 0);
+	return display_change_state(s_bits);
 }
 
 API int pm_lock_state(unsigned int s_bits, unsigned int flag,
-		      unsigned int timeout)
+		    unsigned int timeout)
 {
-	switch (s_bits) {
-	case LCD_NORMAL:
-	case LCD_DIM:
-	case LCD_OFF:
-		break;
-	default:
-		return -1;
-	}
-	if (flag & GOTO_STATE_NOW)
-		/* if the flag is true, go to the locking state directly */
-		s_bits = s_bits | (s_bits << SHIFT_CHANGE_STATE);
-	if (flag & HOLD_KEY_BLOCK)
-		s_bits = s_bits | (1 << SHIFT_HOLD_KEY_BLOCK);
-
-	return send_msg(s_bits, timeout);
+	return display_lock_state(s_bits, flag, timeout);
 }
 
 API int pm_unlock_state(unsigned int s_bits, unsigned int flag)
 {
-	switch (s_bits) {
-	case LCD_NORMAL:
-	case LCD_DIM:
-	case LCD_OFF:
-		break;
-	default:
-		return -1;
-	}
-
-	s_bits = (s_bits << SHIFT_UNLOCK);
-	s_bits = (s_bits | (flag << SHIFT_UNLOCK_PARAMETER));
-	return send_msg(s_bits, 0);
+	return display_unlock_state(s_bits, flag);
 }
 
