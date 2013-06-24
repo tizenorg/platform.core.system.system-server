3328afbc71195e93e1ba34119db247a708e8b5ba
jy910.yun <jy910.yun@samsung.com>
2013-06-24 20:44:09 +0900

    embody 'haptic' in libdeviced
    Change-Id: I4901c5cc307615214bd8757e0b40c0f6e0ea3dd6
    Signed-off-by: Krzysztof Sasiak <k.sasiak@samsung.com>

---
+++ src/deviced/haptic-module.h
@@ -0,0 +1,87 @@
+/*
+ * deviced
+ * Copyright (c) 2012 - 2013 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __HAPTIC_MODULE_H__
+#define __HAPTIC_MODULE_H__
+
+/**
+ * @brief Enumerations of device id for the Haptic Module API.
+ * @details We support two motors now.
+ */
+typedef enum {
+    HAPTIC_MODULE_DEVICE_0 = 0x0,             /**< 1st motor */
+    HAPTIC_MODULE_DEVICE_1 = 0x1,             /**< 2nd motor */
+    HAPTIC_MODULE_DEVICE_ALL = 0x4,           /**< both of them */
+} haptic_module_device;
+
+/**
+ * @brief Enumerations of priority level for the Haptic Module API.
+ */
+typedef enum
+{
+    HAPTIC_MODULE_PRIORITY_MIN = 0,        /**< Minimum effect priority for developers (default) */
+    HAPTIC_MODULE_PRIORITY_MIDDLE,         /**< Maximum effect priority for developers */
+    HAPTIC_MODULE_PRIORITY_HIGH,           /**< Maximum effect priority for OEMs */
+} haptic_module_priority;
+
+/**
+ * @brief Enumerations of feedback level for the Haptic Module API.
+ * @details Haptic level means vibration power (intensity).
+ */
+typedef enum
+{
+    HAPTIC_MODULE_FEEDBACK_MIN = 0,
+    HAPTIC_MODULE_FEEDBACK_MAX = 100,
+} haptic_module_feedback;
+
+/**
+ * @brief Enumerations of iteration count for the Haptic Module API.
+ */
+typedef enum
+{
+    HAPTIC_MODULE_ITERATION_ONCE = 1,
+    HAPTIC_MODULE_ITERATION_INFINITE = 256,
+} haptic_module_iteration;
+
+/**
+ * @brief Enumerations of effect or device state for the Haptic Module API.
+ */
+typedef enum
+{
+    HAPTIC_MODULE_STATE_PLAYING = 0,
+    HAPTIC_MODULE_STATE_STOP,
+} haptic_module_state;
+
+/* Error and Return value codes */
+#define HAPTIC_MODULE_ERROR_NONE         						 0
+#define HAPTIC_MODULE_NOT_INITIALIZED    						-1
+#define HAPTIC_MODULE_ALREADY_INITIALIZED						-2
+#define HAPTIC_MODULE_INVALID_ARGUMENT   						-3
+#define HAPTIC_MODULE_OPERATION_FAILED   						-4
+#define HAPTIC_MODULE_NOT_SUPPORTED								-5
+
+/**
+ * @par Description:
+ *      effect element for haptic module.
+ */
+typedef struct {
+	int haptic_duration; /**< Duration of the effect element in millisecond */
+	int haptic_level;	 /**< Level of the effect element (0 ~ 100) */
+} haptic_module_effect_element;
+
+#endif  /* __HAPTIC_MODULE_H__ */
---
+++ src/deviced/haptic-plugin-intf.h
@@ -0,0 +1,45 @@
+/*
+ * deviced
+ * Copyright (c) 2012 - 2013 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __HAPTIC_PLUGIN_INTF_H__
+#define __HAPTIC_PLUGIN_INTF_H__
+
+#include "haptic-module.h"
+
+struct haptic_ops {
+	int (*get_device_count) (int*);
+	int (*open_device) (int, int*);
+	int (*close_device) (int);
+	int (*vibrate_monotone) (int, int, int, int, int*);
+	int (*vibrate_file) (int, const char*, int, int, int, int*);
+	int (*vibrate_buffer) (int, const unsigned char*, int, int, int, int*);
+	int (*stop_effect) (int, int);
+	int (*stop_all_effects) (int);
+	int (*pause_effect) (int, int);
+	int (*resume_effect) (int, int);
+	int (*get_effect_state) (int, int, int*);
+	int (*create_effect) (unsigned char*, int, haptic_module_effect_element*, int);
+	int (*save_effect) (const unsigned char*, int, const char*);
+	int (*get_file_duration) (int, const char*, int*);
+	int (*get_buffer_duration) (int, const unsigned char*, int*);
+	int (*convert_binary) (const unsigned char*, int, const char*);
+};
+
+const struct haptic_ops *get_haptic_plugin_interface();
+
+#endif	/* __HAPTIC_PLUGIN_INTF_H__ */
