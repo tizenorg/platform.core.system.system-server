tizenorg/platform.core.system.deviced

when called display_release_brightness, also update VCONFKEY_PM_CURRENT_BRIGHTNESS value

src/shared/display.c
@@ -193,6 +193,9 @@ API int display_release_brightness(void)
 
 	if (auto_brt_state == SETTING_BRIGHTNESS_AUTOMATIC_OFF) {
 		device_set_property(DEVICE_TYPE_DISPLAY, PROP_DISPLAY_BRIGHTNESS, setting_val);
+		if (vconf_set_int(VCONFKEY_PM_CURRENT_BRIGHTNESS, setting_val) != 0) {
+			PRT_ERR("Failed to set VCONFKEY_PM_CURRENT_BRIGHTNESS value");
+		}
 	} else if (auto_brt_state == SETTING_BRIGHTNESS_AUTOMATIC_PAUSE) {
 		PRT_DBG("Auto brightness will be enable");
 		vconf_set_int(VCONFKEY_SETAPPL_BRIGHTNESS_AUTOMATIC_INT, SETTING_BRIGHTNESS_AUTOMATIC_ON);

tizenorg/platform.core.system.system-server

Change-Id: Ic07de48bd257ca4be6dad977b45559043f7b7c13
Signed-off-by: Krzysztof Sasiak <k.sasiak@samsung.com>

src/shared/display.c
@@ -193,6 +193,9 @@ API int display_release_brightness(void)
 
 	if (auto_brt_state == SETTING_BRIGHTNESS_AUTOMATIC_OFF) {
 		device_set_property(DEVICE_TYPE_DISPLAY, PROP_DISPLAY_BRIGHTNESS, setting_val);
+		if (vconf_set_int(VCONFKEY_PM_CURRENT_BRIGHTNESS, setting_val) != 0) {
+			_E("Failed to set VCONFKEY_PM_CURRENT_BRIGHTNESS value");
+		}
 	} else if (auto_brt_state == SETTING_BRIGHTNESS_AUTOMATIC_PAUSE) {
 		_D("Auto brightness will be enable");
 		vconf_set_int(VCONFKEY_SETAPPL_BRIGHTNESS_AUTOMATIC_INT, SETTING_BRIGHTNESS_AUTOMATIC_ON);