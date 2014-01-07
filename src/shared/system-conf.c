tizenorg/platform.core.system.deviced

embody 'sysman' in libdeviced
Signed-off-by: giyeol.ok <giyeol.ok@samsung.com>

src/shared/system-conf.c
@@ -1,0 +1,235 @@
+/*
+ *  libdeviced
+ *
+ * Copyright (c) 2012 - 2013 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <limits.h>
+
+#include "log.h"
+#include "system-priv.h"
+#include "dd-system.h"
+
+#define PERMANENT_DIR		"/tmp/permanent"
+#define VIP_DIR			"/tmp/vip"
+
+#define OOMADJ_SET		"oomadj_set"
+#define PROCESS_GROUP_SET	"process_group_set"
+#define PROCESS_VIP		"process_vip"
+#define PROCESS_PERMANENT	"process_permanent"
+
+enum mp_entry_type {
+	MP_VIP,
+	MP_PERMANENT,
+	MP_NONE
+};
+
+int util_oomadj_set(int pid, int oomadj_val)
+{
+	char buf1[255];
+	char buf2[255];
+
+	snprintf(buf1, sizeof(buf1), "%d", pid);
+	snprintf(buf2, sizeof(buf2), "%d", oomadj_val);
+	return system_call_predef_action(OOMADJ_SET, 2, buf1, buf2);
+}
+
+int util_process_group_set(const char* name, int pid)
+{
+	char buf[255];
+
+	if (strncmp(PROCESS_VIP, name, strlen(name)) != 0 &&
+	    strncmp(PROCESS_PERMANENT, name, strlen(name)) != 0) {
+		PRT_TRACE_ERR("fail to insert at %s group", name);
+		return -1;
+	}
+
+	snprintf(buf, sizeof(buf), "%d", pid);
+	PRT_TRACE_ERR("pid(%d) is inserted at vip", pid);
+
+	return system_call_predef_action(PROCESS_GROUP_SET, 2, buf, name);
+}
+
+API int system_conf_set_mempolicy_bypid(int pid, enum mem_policy mempol)
+{
+	if (pid < 1)
+		return -1;
+
+	int oomadj_val = 0;
+
+	switch (mempol) {
+	case OOM_LIKELY:
+		oomadj_val = 1;
+		break;
+	case OOM_IGNORE:
+		oomadj_val = -17;
+		break;
+	default:
+		return -1;
+	}
+
+	return util_oomadj_set(pid, oomadj_val);
+}
+
+API int system_conf_set_mempolicy(enum mem_policy mempol)
+{
+	return system_conf_set_mempolicy_bypid(getpid(), mempol);
+}
+
+static int already_permanent(int pid)
+{
+	char buf[BUFF_MAX];
+
+	snprintf(buf, BUFF_MAX, "%s/%d", PERMANENT_DIR, pid);
+
+	if (access(buf, R_OK) == 0) {
+		PRT_TRACE("already_permanent process : %d", pid);
+		return 1;
+	}
+	return 0;
+}
+
+static int copy_cmdline(int pid)
+{
+	char buf[PATH_MAX];
+	char filepath[PATH_MAX];
+	int fd;
+	int cnt;
+	int r;
+
+	if (access(PERMANENT_DIR, R_OK) < 0) {
+		PRT_TRACE("no predefined matrix dir = %s, so created", PERMANENT_DIR);
+		r = mkdir(PERMANENT_DIR, 0777);
+		if(r < 0) {
+			PRT_TRACE_ERR("permanent directory mkdir is failed");
+			return -1;
+		}
+	}
+
+	snprintf(filepath, PATH_MAX, "/proc/%d/cmdline", pid);
+
+	fd = open(filepath, O_RDONLY);
+	if (fd == -1) {
+		PRT_TRACE("Failed to open");
+		return -1;
+	}
+
+	cnt = read(fd, buf, PATH_MAX);
+	close(fd);
+
+	if (cnt <= 0) {
+		/* Read /proc/<pid>/cmdline error */
+		PRT_TRACE("Failed to read");
+		return -1;
+	}
+
+	snprintf(filepath, PATH_MAX, "%s/%d", PERMANENT_DIR, pid);
+
+	fd = open(filepath, O_CREAT | O_WRONLY, 0644);
+	if (fd == -1) {
+		PRT_TRACE("Failed to open");
+		return -1;
+	}
+
+	if (write(fd, buf, cnt) == -1) {
+		PRT_TRACE("Failed to write");
+		close(fd);
+		return -1;
+	}
+	close(fd);
+
+	return 0;
+}
+
+API int system_conf_set_vip(int pid)
+{
+	char buf[BUFF_MAX];
+	int fd;
+	int r;
+
+	if (pid < 1)
+		return -1;
+
+	if (access(VIP_DIR, R_OK) < 0) {
+		PRT_TRACE("no predefined matrix dir = %s, so created", VIP_DIR);
+		r = mkdir(VIP_DIR, 0777);
+		if(r < 0) {
+			PRT_TRACE_ERR("sysconf_set_vip vip mkdir is failed");
+			return -1;
+		}
+	}
+
+	snprintf(buf, BUFF_MAX, "%s/%d", VIP_DIR, pid);
+	fd = open(buf, O_CREAT | O_RDWR, 0644);
+	if (fd < 0) {
+		PRT_TRACE_ERR("sysconf_set_vip fd open failed");
+		return -1;
+	}
+	close(fd);
+	if (util_process_group_set(PROCESS_VIP, pid) < 0) {
+		PRT_TRACE_ERR("set vip failed");
+		return -1;
+	}
+
+	return 0;
+}
+
+API int system_conf_is_vip(int pid)
+{
+	if (pid < 1)
+		return -1;
+
+	char buf[BUFF_MAX];
+
+	snprintf(buf, BUFF_MAX, "%s/%d", VIP_DIR, pid);
+
+	if (access(buf, R_OK) == 0)
+		return 1;
+	else
+		return 0;
+}
+
+API int system_conf_set_permanent_bypid(int pid)
+{
+	int fd;
+	if (already_permanent(pid))
+		goto MEMPOL_SET;
+
+	if (copy_cmdline(pid) < 0)
+		return -1;
+
+	if (util_process_group_set(PROCESS_PERMANENT, pid) < 0) {
+		PRT_TRACE_ERR("set vip failed");
+		return -1;
+	}
+
+ MEMPOL_SET:
+	util_oomadj_set(pid, -17);
+
+	return 0;
+}
+
+API int system_conf_set_permanent()
+{
+	pid_t pid = getpid();
+	return system_conf_set_permanent_bypid(pid);
+}

src/shared/system-util.c
@@ -1,0 +1,133 @@
+/*
+ *  deviced
+ *
+ * Copyright (c) 2012 - 2013 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <poll.h>
+
+#include <vconf.h>
+#include "log.h"
+#include "dd-system.h"
+#include "system-priv.h"
+
+API int system_get_pid(const char *execpath)
+{
+	DIR *dp;
+	struct dirent *dentry;
+	int pid = -1, fd;
+	char buf[BUFF_MAX];
+	char buf2[BUFF_MAX];
+
+	dp = opendir("/proc");
+	if (!dp) {
+		PRT_TRACE_ERR("open /proc");
+		return -1;
+	}
+
+	while ((dentry = readdir(dp)) != NULL) {
+		if (!isdigit(dentry->d_name[0]))
+			continue;
+
+		pid = atoi(dentry->d_name);
+
+		snprintf(buf, BUFF_MAX, "/proc/%d/cmdline", pid);
+		fd = open(buf, O_RDONLY);
+		if (fd < 0)
+			continue;
+		if (read(fd, buf2, BUFF_MAX) < 0) {
+			close(fd);
+			continue;
+		}
+		close(fd);
+
+		if (!strcmp(buf2, execpath)) {
+			closedir(dp);
+			return pid;
+		}
+	}
+
+	errno = ESRCH;
+	closedir(dp);
+	return -1;
+}
+
+API int system_get_cmdline_name(pid_t pid, char *cmdline, size_t cmdline_size)
+{
+	int fd, ret;
+	char buf[PATH_MAX + 1];
+	char *filename;
+
+	snprintf(buf, sizeof(buf), "/proc/%d/cmdline", pid);
+	fd = open(buf, O_RDONLY);
+	if (fd < 0) {
+		errno = ESRCH;
+		return -1;
+	}
+
+	ret = read(fd, buf, PATH_MAX);
+	close(fd);
+	buf[PATH_MAX] = '\0';
+
+	filename = strrchr(buf, '/');
+	if (filename == NULL)
+		filename = buf;
+	else
+		filename = filename + 1;
+
+	if (cmdline_size < strlen(filename) + 1) {
+		errno = EOVERFLOW;
+		return -1;
+	}
+
+	strncpy(cmdline, filename, cmdline_size - 1);
+	cmdline[cmdline_size - 1] = '\0';
+	return 0;
+}
+
+API int system_get_apppath(pid_t pid, char *app_path, size_t app_path_size)
+{
+	char buf[PATH_MAX];
+	int ret;
+
+	snprintf(buf, PATH_MAX, "/proc/%d/exe", pid);
+	if (app_path == NULL
+	    || (ret = readlink(buf, app_path, app_path_size)) == -1)
+		return -1;
+	if (app_path_size == ret) {
+		app_path[ret - 1] = '\0';
+		errno = EOVERFLOW;
+		return -1;
+	}
+
+	app_path[ret] = '\0';
+	return 0;
+}
+

tizenorg/platform.core.system.system-server

Change-Id: Ic9eb8f1643288f36fa2fcf03fc0fc2207749bf21
Signed-off-by: Krzysztof Sasiak <k.sasiak@samsung.com>

src/shared/system-conf.c
@@ -1,0 +1,235 @@
+/*
+ *  libdeviced
+ *
+ * Copyright (c) 2012 - 2013 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <limits.h>
+
+#include "log.h"
+#include "system-priv.h"
+#include "dd-system.h"
+
+#define PERMANENT_DIR		"/tmp/permanent"
+#define VIP_DIR			"/tmp/vip"
+
+#define OOMADJ_SET		"oomadj_set"
+#define PROCESS_GROUP_SET	"process_group_set"
+#define PROCESS_VIP		"process_vip"
+#define PROCESS_PERMANENT	"process_permanent"
+
+enum mp_entry_type {
+	MP_VIP,
+	MP_PERMANENT,
+	MP_NONE
+};
+
+int util_oomadj_set(int pid, int oomadj_val)
+{
+	char buf1[255];
+	char buf2[255];
+
+	snprintf(buf1, sizeof(buf1), "%d", pid);
+	snprintf(buf2, sizeof(buf2), "%d", oomadj_val);
+	return system_call_predef_action(OOMADJ_SET, 2, buf1, buf2);
+}
+
+int util_process_group_set(const char* name, int pid)
+{
+	char buf[255];
+
+	if (strncmp(PROCESS_VIP, name, strlen(name)) != 0 &&
+	    strncmp(PROCESS_PERMANENT, name, strlen(name)) != 0) {
+		_E("fail to insert at %s group", name);
+		return -1;
+	}
+
+	snprintf(buf, sizeof(buf), "%d", pid);
+	_E("pid(%d) is inserted at vip", pid);
+
+	return system_call_predef_action(PROCESS_GROUP_SET, 2, buf, name);
+}
+
+API int system_conf_set_mempolicy_bypid(int pid, enum mem_policy mempol)
+{
+	if (pid < 1)
+		return -1;
+
+	int oomadj_val = 0;
+
+	switch (mempol) {
+	case OOM_LIKELY:
+		oomadj_val = 1;
+		break;
+	case OOM_IGNORE:
+		oomadj_val = -17;
+		break;
+	default:
+		return -1;
+	}
+
+	return util_oomadj_set(pid, oomadj_val);
+}
+
+API int system_conf_set_mempolicy(enum mem_policy mempol)
+{
+	return system_conf_set_mempolicy_bypid(getpid(), mempol);
+}
+
+static int already_permanent(int pid)
+{
+	char buf[BUFF_MAX];
+
+	snprintf(buf, BUFF_MAX, "%s/%d", PERMANENT_DIR, pid);
+
+	if (access(buf, R_OK) == 0) {
+		_D("already_permanent process : %d", pid);
+		return 1;
+	}
+	return 0;
+}
+
+static int copy_cmdline(int pid)
+{
+	char buf[PATH_MAX];
+	char filepath[PATH_MAX];
+	int fd;
+	int cnt;
+	int r;
+
+	if (access(PERMANENT_DIR, R_OK) < 0) {
+		_D("no predefined matrix dir = %s, so created", PERMANENT_DIR);
+		r = mkdir(PERMANENT_DIR, 0777);
+		if(r < 0) {
+			_E("permanent directory mkdir is failed");
+			return -1;
+		}
+	}
+
+	snprintf(filepath, PATH_MAX, "/proc/%d/cmdline", pid);
+
+	fd = open(filepath, O_RDONLY);
+	if (fd == -1) {
+		_D("Failed to open");
+		return -1;
+	}
+
+	cnt = read(fd, buf, PATH_MAX);
+	close(fd);
+
+	if (cnt <= 0) {
+		/* Read /proc/<pid>/cmdline error */
+		_D("Failed to read");
+		return -1;
+	}
+
+	snprintf(filepath, PATH_MAX, "%s/%d", PERMANENT_DIR, pid);
+
+	fd = open(filepath, O_CREAT | O_WRONLY, 0644);
+	if (fd == -1) {
+		_D("Failed to open");
+		return -1;
+	}
+
+	if (write(fd, buf, cnt) == -1) {
+		_D("Failed to write");
+		close(fd);
+		return -1;
+	}
+	close(fd);
+
+	return 0;
+}
+
+API int system_conf_set_vip(int pid)
+{
+	char buf[BUFF_MAX];
+	int fd;
+	int r;
+
+	if (pid < 1)
+		return -1;
+
+	if (access(VIP_DIR, R_OK) < 0) {
+		_D("no predefined matrix dir = %s, so created", VIP_DIR);
+		r = mkdir(VIP_DIR, 0777);
+		if(r < 0) {
+			_E("sysconf_set_vip vip mkdir is failed");
+			return -1;
+		}
+	}
+
+	snprintf(buf, BUFF_MAX, "%s/%d", VIP_DIR, pid);
+	fd = open(buf, O_CREAT | O_RDWR, 0644);
+	if (fd < 0) {
+		_E("sysconf_set_vip fd open failed");
+		return -1;
+	}
+	close(fd);
+	if (util_process_group_set(PROCESS_VIP, pid) < 0) {
+		_E("set vip failed");
+		return -1;
+	}
+
+	return 0;
+}
+
+API int system_conf_is_vip(int pid)
+{
+	if (pid < 1)
+		return -1;
+
+	char buf[BUFF_MAX];
+
+	snprintf(buf, BUFF_MAX, "%s/%d", VIP_DIR, pid);
+
+	if (access(buf, R_OK) == 0)
+		return 1;
+	else
+		return 0;
+}
+
+API int system_conf_set_permanent_bypid(int pid)
+{
+	int fd;
+	if (already_permanent(pid))
+		goto MEMPOL_SET;
+
+	if (copy_cmdline(pid) < 0)
+		return -1;
+
+	if (util_process_group_set(PROCESS_PERMANENT, pid) < 0) {
+		_E("set vip failed");
+		return -1;
+	}
+
+ MEMPOL_SET:
+	util_oomadj_set(pid, -17);
+
+	return 0;
+}
+
+API int system_conf_set_permanent()
+{
+	pid_t pid = getpid();
+	return system_conf_set_permanent_bypid(pid);
+}

src/shared/system-util.c
@@ -1,0 +1,133 @@
+/*
+ *  deviced
+ *
+ * Copyright (c) 2012 - 2013 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <poll.h>
+
+#include <vconf.h>
+#include "log.h"
+#include "dd-system.h"
+#include "system-priv.h"
+
+API int system_get_pid(const char *execpath)
+{
+	DIR *dp;
+	struct dirent *dentry;
+	int pid = -1, fd;
+	char buf[BUFF_MAX];
+	char buf2[BUFF_MAX];
+
+	dp = opendir("/proc");
+	if (!dp) {
+		_E("open /proc");
+		return -1;
+	}
+
+	while ((dentry = readdir(dp)) != NULL) {
+		if (!isdigit(dentry->d_name[0]))
+			continue;
+
+		pid = atoi(dentry->d_name);
+
+		snprintf(buf, BUFF_MAX, "/proc/%d/cmdline", pid);
+		fd = open(buf, O_RDONLY);
+		if (fd < 0)
+			continue;
+		if (read(fd, buf2, BUFF_MAX) < 0) {
+			close(fd);
+			continue;
+		}
+		close(fd);
+
+		if (!strcmp(buf2, execpath)) {
+			closedir(dp);
+			return pid;
+		}
+	}
+
+	errno = ESRCH;
+	closedir(dp);
+	return -1;
+}
+
+API int system_get_cmdline_name(pid_t pid, char *cmdline, size_t cmdline_size)
+{
+	int fd, ret;
+	char buf[PATH_MAX + 1];
+	char *filename;
+
+	snprintf(buf, sizeof(buf), "/proc/%d/cmdline", pid);
+	fd = open(buf, O_RDONLY);
+	if (fd < 0) {
+		errno = ESRCH;
+		return -1;
+	}
+
+	ret = read(fd, buf, PATH_MAX);
+	close(fd);
+	buf[PATH_MAX] = '\0';
+
+	filename = strrchr(buf, '/');
+	if (filename == NULL)
+		filename = buf;
+	else
+		filename = filename + 1;
+
+	if (cmdline_size < strlen(filename) + 1) {
+		errno = EOVERFLOW;
+		return -1;
+	}
+
+	strncpy(cmdline, filename, cmdline_size - 1);
+	cmdline[cmdline_size - 1] = '\0';
+	return 0;
+}
+
+API int system_get_apppath(pid_t pid, char *app_path, size_t app_path_size)
+{
+	char buf[PATH_MAX];
+	int ret;
+
+	snprintf(buf, PATH_MAX, "/proc/%d/exe", pid);
+	if (app_path == NULL
+	    || (ret = readlink(buf, app_path, app_path_size)) == -1)
+		return -1;
+	if (app_path_size == ret) {
+		app_path[ret - 1] = '\0';
+		errno = EOVERFLOW;
+		return -1;
+	}
+
+	app_path[ret] = '\0';
+	return 0;
+}
+