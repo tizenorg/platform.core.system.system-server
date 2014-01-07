tizenorg/platform.core.system.deviced

embody 'pm' in libdeviced.

src/shared/display.c
@@ -19,6 +19,11 @@
 #include <stdio.h>
 #include <vconf.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <linux/limits.h>
 #include <device-node.h>
 
 #include "log.h"
@@ -26,6 +31,19 @@
 
 #define DISPLAY_MAX_BRIGHTNESS  100
 #define DISPLAY_MIN_BRIGHTNESS  0
+
+#define SOCK_PATH			"/tmp/pm_sock"
+#define SHIFT_UNLOCK			4
+#define SHIFT_UNLOCK_PARAMETER		12
+#define SHIFT_CHANGE_STATE		8
+#define SHIFT_HOLD_KEY_BLOCK		16
+#define TIMEOUT_RESET_BIT		0x80
+
+struct pwr_msg {
+	pid_t pid;
+	unsigned int cond;
+	unsigned int timeout;
+};
 
 API int display_get_count(void)
 {
@@ -204,4 +222,96 @@ API int display_set_acl_status(int val)
 		return r;
 
 	return 0;
+}
+
+static int send_msg(unsigned int s_bits, unsigned int timeout)
+{
+	int rc = 0;
+	int sock;
+	struct pwr_msg p;
+	struct sockaddr_un remote;
+
+	p.pid = getpid();
+	p.cond = s_bits;
+	p.timeout = timeout;
+
+	sock = socket(AF_UNIX, SOCK_DGRAM, 0);
+	if (sock == -1) {
+		ERR("pm socket() failed");
+		return -1;
+	}
+
+	remote.sun_family = AF_UNIX;
+	if(strlen(SOCK_PATH) >= sizeof(remote.sun_path)) {
+		ERR("socket path is vey long");
+		return -1;
+	}
+	strncpy(remote.sun_path, SOCK_PATH, sizeof(remote.sun_path));
+
+	rc = sendto(sock, (void *)&p, sizeof(p), 0, (struct sockaddr *)&remote,
+		    sizeof(struct sockaddr_un));
+	if (rc == -1) {
+		ERR("pm socket sendto() failed");
+	} else
+		rc = 0;
+
+	close(sock);
+	return rc;
+}
+
+API int display_change_state(unsigned int s_bits)
+{
+	/* s_bits is LCD_NORMAL 0x1, LCD_DIM 0x2, LCD_OFF 0x4, SUSPEND 0x8
+	 * Stage change to NORMAL       0x100
+	 * Stage change to LCDDIM       0x200
+	 * Stage change to LCDOFF       0x400
+	 * Stage change to SLEEP        0x800
+	 * */
+	switch (s_bits) {
+	case LCD_NORMAL:
+	case LCD_DIM:
+	case LCD_OFF:
+	case SUSPEND:
+	case POWER_OFF:
+		break;
+	default:
+		return -1;
+	}
+	return send_msg(s_bits << SHIFT_CHANGE_STATE, 0);
+}
+
+API int display_lock_state(unsigned int s_bits, unsigned int flag,
+		      unsigned int timeout)
+{
+	switch (s_bits) {
+	case LCD_NORMAL:
+	case LCD_DIM:
+	case LCD_OFF:
+		break;
+	default:
+		return -1;
+	}
+	if (flag & GOTO_STATE_NOW)
+		/* if the flag is true, go to the locking state directly */
+		s_bits = s_bits | (s_bits << SHIFT_CHANGE_STATE);
+	if (flag & HOLD_KEY_BLOCK)
+		s_bits = s_bits | (1 << SHIFT_HOLD_KEY_BLOCK);
+
+	return send_msg(s_bits, timeout);
+}
+
+API int display_unlock_state(unsigned int s_bits, unsigned int flag)
+{
+	switch (s_bits) {
+	case LCD_NORMAL:
+	case LCD_DIM:
+	case LCD_OFF:
+		break;
+	default:
+		return -1;
+	}
+
+	s_bits = (s_bits << SHIFT_UNLOCK);
+	s_bits = (s_bits | (flag << SHIFT_UNLOCK_PARAMETER));
+	return send_msg(s_bits, 0);
 }

tizenorg/platform.core.system.system-server

Change-Id: I6c27fc93a205084053814cfc3a59d481e5c3d6c5
Signed-off-by: Krzysztof Sasiak <k.sasiak@samsung.com>

src/shared/display.c
@@ -19,6 +19,11 @@
 #include <stdio.h>
 #include <vconf.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <linux/limits.h>
 #include <device-node.h>
 
 #include "log.h"
@@ -26,6 +31,19 @@
 
 #define DISPLAY_MAX_BRIGHTNESS  100
 #define DISPLAY_MIN_BRIGHTNESS  0
+
+#define SOCK_PATH			"/tmp/pm_sock"
+#define SHIFT_UNLOCK			4
+#define SHIFT_UNLOCK_PARAMETER		12
+#define SHIFT_CHANGE_STATE		8
+#define SHIFT_HOLD_KEY_BLOCK		16
+#define TIMEOUT_RESET_BIT		0x80
+
+struct pwr_msg {
+	pid_t pid;
+	unsigned int cond;
+	unsigned int timeout;
+};
 
 API int display_get_count(void)
 {
@@ -204,4 +222,96 @@ API int display_set_acl_status(int val)
 		return r;
 
 	return 0;
+}
+
+static int send_msg(unsigned int s_bits, unsigned int timeout)
+{
+	int rc = 0;
+	int sock;
+	struct pwr_msg p;
+	struct sockaddr_un remote;
+
+	p.pid = getpid();
+	p.cond = s_bits;
+	p.timeout = timeout;
+
+	sock = socket(AF_UNIX, SOCK_DGRAM, 0);
+	if (sock == -1) {
+		_E("pm socket() failed");
+		return -1;
+	}
+
+	remote.sun_family = AF_UNIX;
+	if(strlen(SOCK_PATH) >= sizeof(remote.sun_path)) {
+		_E("socket path is vey long");
+		return -1;
+	}
+	strncpy(remote.sun_path, SOCK_PATH, sizeof(remote.sun_path));
+
+	rc = sendto(sock, (void *)&p, sizeof(p), 0, (struct sockaddr *)&remote,
+		    sizeof(struct sockaddr_un));
+	if (rc == -1) {
+		_E("pm socket sendto() failed");
+	} else
+		rc = 0;
+
+	close(sock);
+	return rc;
+}
+
+API int display_change_state(unsigned int s_bits)
+{
+	/* s_bits is LCD_NORMAL 0x1, LCD_DIM 0x2, LCD_OFF 0x4, SUSPEND 0x8
+	 * Stage change to NORMAL       0x100
+	 * Stage change to LCDDIM       0x200
+	 * Stage change to LCDOFF       0x400
+	 * Stage change to SLEEP        0x800
+	 * */
+	switch (s_bits) {
+	case LCD_NORMAL:
+	case LCD_DIM:
+	case LCD_OFF:
+	case SUSPEND:
+	case POWER_OFF:
+		break;
+	default:
+		return -1;
+	}
+	return send_msg(s_bits << SHIFT_CHANGE_STATE, 0);
+}
+
+API int display_lock_state(unsigned int s_bits, unsigned int flag,
+		      unsigned int timeout)
+{
+	switch (s_bits) {
+	case LCD_NORMAL:
+	case LCD_DIM:
+	case LCD_OFF:
+		break;
+	default:
+		return -1;
+	}
+	if (flag & GOTO_STATE_NOW)
+		/* if the flag is true, go to the locking state directly */
+		s_bits = s_bits | (s_bits << SHIFT_CHANGE_STATE);
+	if (flag & HOLD_KEY_BLOCK)
+		s_bits = s_bits | (1 << SHIFT_HOLD_KEY_BLOCK);
+
+	return send_msg(s_bits, timeout);
+}
+
+API int display_unlock_state(unsigned int s_bits, unsigned int flag)
+{
+	switch (s_bits) {
+	case LCD_NORMAL:
+	case LCD_DIM:
+	case LCD_OFF:
+		break;
+	default:
+		return -1;
+	}
+
+	s_bits = (s_bits << SHIFT_UNLOCK);
+	s_bits = (s_bits | (flag << SHIFT_UNLOCK_PARAMETER));
+	return send_msg(s_bits, 0);
 }