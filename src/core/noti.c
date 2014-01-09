tizenorg/platform.core.system.deviced

apply device registration
details:
	sysnoti, noti, core, signal, predefine, lowmem, lowbat, device change handler,
	power, bs, process monitor, time handler, cpu, usb, ta, process manager, mmc,
	heynoti, powermanager and vibrator changed

	before changing, to invoke a initialize function, it must call directly in main func.
	So if some function deleted, we have to delete related part on each files.
	but after applying this patchset, we only have to delete just one file regarding function you want delete.

Signed-off-by: jy910.yun <jy910.yun@samsung.com>

src/core/noti.c
@@ -17,6 +17,8 @@
 
 #include <heynoti.h>
 #include "log.h"
+#include "data.h"
+#include "devices.h"
 
 static int noti_fd;
 
@@ -30,22 +32,6 @@ int ss_noti_send(char *filename)
 	return heynoti_publish(filename);
 }
 
-int ss_noti_init()
-{
-	noti_fd = heynoti_init();
-	if (noti_fd < 0) {
-		PRT_TRACE_ERR("heynoti_init error");
-		return -1;
-	}
-
-	if (heynoti_attach_handler(noti_fd) < 0) {
-		PRT_TRACE_ERR("heynoti_attach_handler error");
-		return -1;
-	}
-
-	return 0;
-}
-
 int ss_noti_add(const char *noti, void (*cb) (void *), void *data)
 {
 	if (noti_fd < 0)
@@ -53,3 +39,41 @@ int ss_noti_add(const char *noti, void (*cb) (void *), void *data)
 
 	return heynoti_subscribe(noti_fd, noti, cb, data);
 }
+
+static void noti_init(void *data)
+{
+	struct ss_main_data *ad = (struct ss_main_data*)data;
+
+	if ((ad->noti_fd = heynoti_init()) < 0) {
+		PRT_TRACE_ERR("Hey Notification Initialize failed");
+		return;
+	}
+	if (heynoti_attach_handler(ad->noti_fd) != 0) {
+		PRT_TRACE_ERR("fail to attach hey noti handler");
+		return;
+	}
+
+	noti_fd = heynoti_init();
+	if (noti_fd < 0) {
+		PRT_TRACE_ERR("heynoti_init error");
+		return;
+	}
+
+	if (heynoti_attach_handler(noti_fd) < 0) {
+		PRT_TRACE_ERR("heynoti_attach_handler error");
+		return;
+	}
+}
+
+static void noti_exit(void *data)
+{
+	struct ss_main_data *ad = (struct ss_main_data*)data;
+
+	heynoti_close(ad->noti_fd);
+	heynoti_close(noti_fd);
+}
+
+const struct device_ops noti_device_ops = {
+	.init = noti_init,
+	.exit = noti_exit,
+};

tizenorg/platform.core.system.system-server

Change-Id: I7283b9ed48afbf0c3ba05d383993610a4ac154d5

src/core/noti.c
@@ -17,6 +17,8 @@
 
 #include <heynoti.h>
 #include "log.h"
+#include "data.h"
+#include "devices.h"
 
 static int noti_fd;
 
@@ -30,22 +32,6 @@ int ss_noti_send(char *filename)
 	return heynoti_publish(filename);
 }
 
-int ss_noti_init()
-{
-	noti_fd = heynoti_init();
-	if (noti_fd < 0) {
-		_E("heynoti_init error");
-		return -1;
-	}
-
-	if (heynoti_attach_handler(noti_fd) < 0) {
-		_E("heynoti_attach_handler error");
-		return -1;
-	}
-
-	return 0;
-}
-
 int ss_noti_add(const char *noti, void (*cb) (void *), void *data)
 {
 	if (noti_fd < 0)
@@ -53,3 +39,41 @@ int ss_noti_add(const char *noti, void (*cb) (void *), void *data)
 
 	return heynoti_subscribe(noti_fd, noti, cb, data);
 }
+
+static void noti_init(void *data)
+{
+	struct ss_main_data *ad = (struct ss_main_data*)data;
+
+	if ((ad->noti_fd = heynoti_init()) < 0) {
+		PRT_TRACE_ERR("Hey Notification Initialize failed");
+		return;
+	}
+	if (heynoti_attach_handler(ad->noti_fd) != 0) {
+		PRT_TRACE_ERR("fail to attach hey noti handler");
+		return;
+	}
+
+	noti_fd = heynoti_init();
+	if (noti_fd < 0) {
+		_E("heynoti_init error");
+		return;
+	}
+
+	if (heynoti_attach_handler(noti_fd) < 0) {
+		_E("heynoti_attach_handler error");
+		return;
+	}
+}
+
+static void noti_exit(void *data)
+{
+	struct ss_main_data *ad = (struct ss_main_data*)data;
+
+	heynoti_close(ad->noti_fd);
+	heynoti_close(noti_fd);
+}
+
+const struct device_ops noti_device_ops = {
+	.init = noti_init,
+	.exit = noti_exit,
+};